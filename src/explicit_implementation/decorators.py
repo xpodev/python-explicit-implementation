314d833e2656fa76c3f190b6dee66fa1cfcd357c
Binyamin Y Cohen <biny@xpo.dev>
2025-09-15 20:53:52 +0300

    Fix typing

--- src/explicit_implementation/decorators.py
+++ src/explicit_implementation/decorators.py
@@ -1,4 +1,4 @@
-from typing import Callable, Concatenate, ParamSpec, TypeVar
+from typing import Any, Callable, Concatenate, ParamSpec, TypeVar
 
 
 _P = ParamSpec("_P")
@@ -6,7 +6,7 @@ _T = TypeVar("_T")
 _R = TypeVar("_R")
 
 
-def implements(interface_method: Callable[Concatenate[..., _P], _R]):
+def implements(interface_method: Callable[Concatenate[Any, _P], _R]):
     def mark_implementation(
         implementation_method: Callable[Concatenate[_T, _P], _R]
     ) -> Callable[Concatenate[_T, _P], _R]:
